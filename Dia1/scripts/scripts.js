c5a1c33d177fb3282acabe4ceee964076dcc5bd5
johan <johanbadillomj@gmail.com>
2025-07-18 11:21:13 -0500

    feat: :sparkles: first command

---
+++ Dia1/scripts/scripts.js
@@ -0,0 +1,13 @@
+// ###################################
+//           DIA 1  Explicacion
+// ###################################
+
+
+// Primer comando
+console.log("Hola Mundo");
+
+
+
+
+
+// Creado por : Monsalve Johan - C.C. 1097911956
