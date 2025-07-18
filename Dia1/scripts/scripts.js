d2ae25a8cd5da424610412c548c03f5a49927f79
johan <johanbadillomj@gmail.com>
2025-07-18 12:04:08 -0500

    feat: :sparkles: first explanations and variables

--- Dia1/scripts/scripts.js
+++ Dia1/scripts/scripts.js
@@ -6,7 +6,36 @@
 // Primer comando
 console.log("Hola Mundo");
 
+//number
+variable1= 3.14;
+console.log(variable1);
+console.log(typeof variable1);
 
+
+//contenacion
+palabra1="Campus";
+palabra2="Land";
+palabraCompleta=palabra1+palabra2;
+console.log(palabraCompleta)
+
+
+nombre="johan";
+plata=5500;
+console.log("la persona "+nombre+" tiene un total de plata ahorrada de "+plata+" en el banco de bogota")
+
+
+//booleano
+booleanito1=true;
+booleanito2=false;
+console.log(typeof booleanito1);
+
+
+//condicionales
+if(booleanito2 == true){
+    console.log("Es verdadero")
+}else{
+    console.log("Es falso :sadfeis:")
+}
 
 
 
