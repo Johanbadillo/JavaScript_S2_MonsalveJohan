6990f8b8022d65aedb3369de731a00a08b622eb6
johan <johanbadillomj@gmail.com>
2025-07-19 10:46:12 -0500

    feat: :ambulance: errors in the script

--- Dia1Trabajo/script/script.js
+++ Dia1Trabajo/script/script.js
@@ -1,14 +1,32 @@
-function Opciones() {
+const menuPrincipal=`
+        <h1>Opciones Disponibles</h1>
+        <ul>
+            <li><strong>1.</strong> Registrar dato nuevo</li>
+            <li><strong>2.</strong> Leer datos registrado</li>
+            <li><strong>3.</strong> Leer un dato en especifico</li>
+            <li><strong>4.</strong> Actualizar dato </li>
+            <li><strong>5.</strong> Eliminar dato registrad</li>
+            <li><strong>6.</strong> Regresar al menu principal</li>
+        </ul>
+        `;
+
+function opciones() {
     let opcion = document.getElementById("opcion").value;
     let resultadoDIV = document.getElementById("resultado");
+    let menuPrincipal = document.querySelector(".menuPrincipal");
 
     opcion = Number(opcion);
 
     let menu = '';
     let claseCSS = '';
 
+    resultadoDIV.innerHTML='';
+    resultadoDIV.className='';
+    resultadoDIV.style.display='none';
+
     if (opcion == 1) {
         menu = `
+        <h1>Menu Ingredientes</h1>
         <ul>
             <li><strong>1.</strong> Registrar dato nuevo</li>
             <li><strong>2.</strong> Leer datos registrado</li>
@@ -19,8 +37,12 @@ function Opciones() {
         </ul>
         `;
         claseCSS = 'opcion1';
+        menuPrincipal.innerHTML = menu;
+        menuPrincipal.classList.remove('opcion2', 'opcion3', 'opcion4', 'opcion5', 'opcion6', 'error');
+        menuPrincipal.classList.add(claseCSS);
     } else if (opcion == 2) {
         menu = `
+        <h1>Historial Ingredientes</h1>
         <ul>
             <li><strong>1.</strong> Registrar dato nuevo</li>
             <li><strong>2.</strong> Leer datos registrado</li>
@@ -31,9 +53,13 @@ function Opciones() {
         </ul>
         `;
         claseCSS = 'opcion2'
+        menuPrincipal.innerHTML = menu;
+        menuPrincipal.classList.remove('opcion1', 'opcion3', 'opcion4', 'opcion5', 'opcion6', 'error');
+        menuPrincipal.classList.add(claseCSS);
     }
     else if (opcion == 3) {
         menu = `
+        <h1>Menu Categorias</h1>
         <ul>
             <li><strong>1.</strong> Registrar dato nuevo</li>
             <li><strong>2.</strong> Leer datos registrado</li>
@@ -44,9 +70,13 @@ function Opciones() {
         </ul>
         `;
         claseCSS = 'opcion3'
+        menuPrincipal.innerHTML = menu;
+        menuPrincipal.classList.remove('opcion2', 'opcion1', 'opcion4', 'opcion5', 'opcion6', 'error');
+        menuPrincipal.classList.add(claseCSS);
     }
     else if (opcion == 4) {
         menu = `
+        <h1>Menu Chefs</h1>
         <ul>
             <li><strong>1.</strong> Registrar dato nuevo</li>
             <li><strong>2.</strong> Leer datos registrado</li>
@@ -57,9 +87,13 @@ function Opciones() {
         </ul>
         `;
         claseCSS = 'opcion4'
+        menuPrincipal.innerHTML = menu;
+        menuPrincipal.classList.remove('opcion2', 'opcion3', 'opcion1', 'opcion5', 'opcion6', 'error');
+        menuPrincipal.classList.add(claseCSS);
     }
     else if (opcion == 5) {
         menu = `
+        <h1>Menu Hamburguesas</h1>
         <ul>
             <li><strong>1.</strong> Registrar dato nuevo</li>
             <li><strong>2.</strong> Leer datos registrado</li>
@@ -70,38 +104,28 @@ function Opciones() {
         </ul>
         `;
         claseCSS = 'opcion5'
+        menuPrincipal.innerHTML = menu;
+        menuPrincipal.classList.remove('opcion2', 'opcion3', 'opcion4', 'opcion1', 'opcion6', 'error');
+        menuPrincipal.classList.add(claseCSS);
     } else if (opcion == 6) {
-        menu = `
-        <h2>Regresando al menu anterior</h2>
-        `;
-        claseCSS = 'opcion5'
+        menuPrincipal.innerHTML = menuPrincipal;
+        claseCSS = 'menuPrincipal'
+        menuPrincipal.classList.remove('opcion2', 'opcion3', 'opcion4', 'opcion5', 'opcion1','opcion6', 'error');
+        resultadoDIV.innerHTML=menuPrincipal
     }else {
-        menu = `
-        <ul>
-        <h2>Opcion no valida</h2>
-        <p>Las opciones disponibles son:</p>
-            <li><strong>1.</strong> Registrar dato nuevo</li>
-            <li><strong>2.</strong> Leer datos registrado</li>
-            <li><strong>3.</strong> Leer un dato en especifico</li>
-            <li><strong>4.</strong> Actualizar dato </li>
-            <li><strong>5.</strong> Eliminar dato registrad</li>
-            <li><strong>6.</strong> Regresar al menu principal</li>
-        `;
+        menu = `<h2>Opción no válida</h2>
+        <p>Por favor, seleccione una opción entre 1 y 6.</p>`;
+        claseCSS = 'error';
+        resultadoDIV.innerHTML = menu;
+        resultadoDIV.classList.add(claseCSS);
+        resultadoDIV.style.display = 'flex';;
         claseCSS = 'error'
     }
-    resultadoDIV.className = 'resultado';
-
-    resultadoDIV.innerHTML = menu;
-    resultadoDIV.classList.add(claseCSS);
-
-    resultadoDIV.style.display = 'flex';
-
-    resultadoDIV.scrollIntoView({ behavior: 'smooth' });
 
 }
 
 document.getElementById('opcion').addEventListener('keypress', function (event) {
     if (event.key === 'Enter') {
-        Opciones();
+        opciones();
     }
 });
