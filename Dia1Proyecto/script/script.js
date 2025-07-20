f1b1411a56f1516f7c6a9fd4d8c6a8a371ec9454
johan <johanbadillomj@gmail.com>
2025-07-19 22:15:19 -0500

    feat: :sparkles: udpate en js

--- Dia1Proyecto/script/script.js
+++ Dia1Proyecto/script/script.js
@@ -1,5 +1,21 @@
-let gastos=[];
-let cat=['comida','transporte','entretenimiento','salud','ropa','tecnologia','hogar','otros'];
-let filtro='todos';
+let gastos = [];
+let cat = ['comida', 'transporte', 'entretenimiento', 'salud', 'ropa', 'tecnologia', 'hogar', 'otros'];
+let filtro = 'todos';
 
+function mostrarmenu(id){
+    document.getElementById('menu').classList.add('oculto');
+    document.getElementById('Registrar').classList.add('oculto');
+    document.getElementById('gastos').classList.add('oculto');
+    document.getElementById('totales').classList.add('oculto');
+    document.getElementById('reportes').classList.add('oculto');
 
+    document.getElementById(id).classList.remove('oculto');
+}
+
+function volvermenu(){
+    document.getElementById('Registrar').classList.add('oculto');
+    document.getElementById('gastos').classList.add('oculto');
+    document.getElementById('totales').classList.add('oculto');
+    document.getElementById('reportes').classList.add('oculto');
+    document.getElementById('menu').classList.remove('oculto');
+}
