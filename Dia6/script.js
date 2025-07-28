a3ca6292787437a1b6d549183b853619b0b1f009
johan <johanbadillomj@gmail.com>
2025-07-28 16:06:14 -0500

    feat: :sparkles: fix a bug

--- Dia6/script.js
+++ Dia6/script.js
@@ -7,29 +7,6 @@ function buscarPersonaje() {
   let totalPaginas = 42;
   let personajeEncontrado = false;
 
-  function recolectarNombres(pagina) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://rickandmortyapi.com/api/character?page=' + pagina, true);
-
-    xhr.onload = function () {
-      if (this.readyState == 4 && this.status == 200) {
-        const data = JSON.parse(this.response);
-        for (let i = 0; i < data.results.length; i++) {
-          nombresPersonajes += data.results[i].name + '\n';
-        }
-        if (pagina < totalPaginas) {
-          recolectarNombres(pagina + 1);
-        } else {
-          recolectaCompleta = true;
-          alert('Personajes disponibles:\n\n' + nombresPersonajes);
-          continuarBusqueda();
-        }
-      }
-    };
-
-    xhr.send();
-  }
-
   function buscarEnPagina(pagina) {
     let xhr = new XMLHttpRequest();
     xhr.open('GET', 'https://rickandmortyapi.com/api/character?page=' + pagina, true);
@@ -61,6 +38,5 @@ function buscarPersonaje() {
 
   buscarEnPagina(paginaActual);
 }
-recolectarNombres(paginaActual);
 
 buscarPersonaje();
