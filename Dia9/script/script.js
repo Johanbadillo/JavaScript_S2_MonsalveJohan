7ed0f7f79950a4f56fa9194d48e2ae864d81262d
johan <johanbadillomj@gmail.com>
2025-07-30 17:30:56 -0500

    feat: :sparkles: add js

---
+++ Dia9/script/script.js
@@ -0,0 +1,69 @@
+const pokemonName = document.querySelector('.nombre');
+const pokemonNumber = document.querySelector('#numero');
+const pokemonImage = document.querySelector('.imgPokemon');
+
+const form = document.querySelector('.formulario');
+const input = document.querySelector('#busqueda');
+const buttonPrev = document.querySelector('.atras');
+const buttonNext = document.querySelector('.siguiente');
+
+let searchPokemon = 1;
+
+const fetchPokemon = async (pokemon) => {
+
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+
+
+    if (APIResponse.status == 200) {
+        const data = await APIResponse.json();
+        return data;
+    }
+}
+
+const renderPokemon = async (pokemon) => {
+    pokemonName.innerHTML = 'Loading...';
+    pokemonNumber.innerHTML = '';
+
+    const data = await fetchPokemon(pokemon);
+
+    if (data) {
+        pokemonImage.style.display = 'block';
+        pokemonName.innerHTML = data.name;
+        pokemonNumber.innerHTML = data.id;
+        pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+        audioPokemon.src = data.cries.latest;
+        input.value = '';
+        searchPokemon = data.id;
+    } else {
+        pokemonName.innerHTML = "No hay";
+        pokemonNumber.innerHTML = '';
+    }
+
+}
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    renderPokemon(input.value.toLowerCase());
+})
+
+buttonPrev.addEventListener('click', () => {
+    if (searchPokemon > 1) {
+        searchPokemon -= 1;
+        renderPokemon(searchPokemon);
+    }
+});
+
+buttonNext.addEventListener('click', () => {
+
+    searchPokemon += 1;
+    renderPokemon(searchPokemon);
+
+});
+
+document.document.querySelector('.formulario').addEventListener('keypress', function (event) {
+    if (event.key === 'Enter') {
+        Opciones();
+    }
+});
+
+renderPokemon(searchPokemon);
