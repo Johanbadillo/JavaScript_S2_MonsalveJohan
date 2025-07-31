4262168a029c159818f0e75f639ff084bd6cd995
johan <johanbadillomj@gmail.com>
2025-07-31 11:13:34 -0500

    fix: :sparkles: fix in the img yhe api

--- Dia9/script/script.js
+++ Dia9/script/script.js
@@ -30,7 +30,7 @@ const renderPokemon = async (pokemon) => {
         pokemonImage.style.display = 'block';
         pokemonName.innerHTML = data.name;
         pokemonNumber.innerHTML = data.id;
-        pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+        pokemonImage.src = data['sprites']['front_default'];
         audioPokemon.src = data.cries.latest;
         input.value = '';
         searchPokemon = data.id;
