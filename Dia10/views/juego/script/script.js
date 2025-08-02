dfd789882c29863bba2fa733d30897a15c5c4897
johan <johanbadillomj@gmail.com>
2025-08-02 16:05:20 -0500

    feat: :sparkles: add funtion de calback

--- Dia10/views/juego/script/script.js
+++ Dia10/views/juego/script/script.js
@@ -1,27 +1,53 @@
+let deckId = null;
+let cartaJugador = null;
+let cartaCrupier = null;
+let cartasOcultas = null;
+let victorias = 0;
+let derrotas = 0;
+
 const alta = document.getElementById('alta');
 const baja = document.getElementById('baja');
 const btn = document.getElementById('seleccion')
 
-function empezar(){
+function empezar() {
     btn.style.display = 'none';
 }
 
-function empezar(){
+function empezar() {
     btn.style.display = 'none';
 }
 
-alta.addEventListener('click',empezar);
-baja.addEventListener('click',empezar);
+alta.addEventListener('click', empezar);
+baja.addEventListener('click', empezar);
 
 
-let deckId= null;
-let cartaJugador = null;
-let cartaCrupier = null;
-
-function defValores(valores){
-    if(valores === "ACE") return 1;
-    if(valores === "JACK") return 11;
-    if(valores === "QUEEN") return 12;
-    if(valores === "KING") return 13;
+function defValores(valores) {
+    if (valores === "ACE") return 1;
+    if (valores === "JACK") return 11;
+    if (valores === "QUEEN") return 12;
+    if (valores === "KING") return 13;
     return parseInt(valores)
 }
+
+
+function llamar() {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET",url,true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            const daticos = JSON.parse(xhr.responseText);
+            data(daticos);
+        }
+    };
+    xhr.send();
+}
+function inicio(){
+    const url = `https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`;
+    console.log(url)
+    llamar(url, function(daticos){
+        deckId= daticos.deck_Id;
+        console.log(daticos);
+        console.log(deckId)
+    })
+}
+
